7d6d31b168577583c3ca72cd1b6855b49d9a290d
mussawirimam <mussawirimam@gmail.com>
2022-09-05 14:37:16 -0400

    Python String1

---
+++ PythonString/pythonstrings.py
@@ -0,0 +1,6 @@
+#String are Arrays
+#Like many other popular programming languages, 
+# strings in Python are arrays of bytes representing unicode characters.
+# However, Python does not have a character data type, a single character is simply a string with a length of 1.
+a = "Hello, World!"
+print(a[1]) #Square brackets can be used to access elements of the string.

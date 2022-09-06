27769a6940558eff30ab351f56fb734248229f17
mussawirimam <mussawirimam@gmail.com>
2022-09-06 00:02:22 -0400

    "string modifications"

---
+++ StringSlicing/negativeIndexing.py
@@ -0,0 +1,9 @@
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string:
+# Example
+# Get the characters:
+# From: "o" in "World!" (position -5)
+# To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
---
+++ StringSlicing/stringSlicingtwo.py
@@ -0,0 +1,7 @@
+# Slice From the Start
+# By leaving out the start index, the range will start at the first character:
+# Example
+# Get the characters from the start to position 5 (not included):
+
+b = "Hello, World!"
+print(b[:5])

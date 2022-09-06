f502f51b31f17cb788f0678e33b3dd49f0a6457e
mussawirimam <mussawirimam@gmail.com>
2022-09-06 16:38:12 -0400

    String manipluation

---
+++ StringModify/StringMethods.py
@@ -0,0 +1,18 @@
+# Upper Case
+# Example
+# The upper() method returns the string in upper case:
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
